4c55451a1478b399e22a8c0293b9d37b68ec5aaa
Vladi Leshko <leshkovladik@gmail.com>
2022-08-10 15:40:53 +0300

    fixed bottom button on index and both buttons on template

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,25 @@ function menuOnClick() {
     document.getElementById("menu-bg").classList.toggle("change-bg");
 };
 
-// Получить модальный
 var modal = document.getElementById("modalWindow");
-
-// Получить кнопку, которая открывает модальный
-var btn = document.getElementById("myBtn");
-
-// Получить элемент <span>, который закрывает модальный
+var topBtn = document.getElementById("topPhoneButton");
+var bottomBtn = document.getElementById("bottomPhoneButton");
 var span = document.getElementsByClassName("closeButton")[0];
 
-// Когда пользователь нажимает на кнопку, откройте модальный
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+topBtn.onclick = function() {
+    modal.style.display = "block";
+};
 
-// Когда пользователь нажимает на <span> (x), закройте модальное окно
+bottomBtn.onclick = function() {
+    modal.style.display = "block";
+};
+
 span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// Когда пользователь щелкает в любом месте за пределами модального, закройте его
-window.onclick = function(event) {
-  if (event.target == modal) {
     modal.style.display = "none";
-  }
-}
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+};
